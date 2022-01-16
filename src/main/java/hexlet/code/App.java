529fb7ebcabbe60c07c032c60d91cc81f5c34e57
qed <qedirast@gmail.com>
2022-01-16 14:49:32 +0700

    Added picocli

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,26 @@
 package hexlet.code;
 
-public class App {
+import hexlet.code.Differ.Differ;
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "differ", mixinStandardHelpOptions = true, version = "differ 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    /**
+     * @return int
+     */
+    @Override
+    public Integer call() throws Exception {
+        String diff = Differ.generate("1", "2");
+        System.out.println(diff);
+        return 0;
     }
 }
---
+++ src/main/java/hexlet/code/Differ/Differ.java
@@ -0,0 +1,9 @@
+package hexlet.code.Differ;
+
+public class Differ {
+
+    public static String generate(String firstFile, String secondFile) {
+        return "result";
+    }
+
+}
