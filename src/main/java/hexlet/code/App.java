ad05c901c30128e52b53f7a7abf8680fde80997f
qed <qedirast@gmail.com>
2022-01-16 14:58:46 +0700

    Added parameters and options

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -9,6 +9,15 @@ import java.util.concurrent.Callable;
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
 
+    @CommandLine.Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+
+    @CommandLine.Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
+    @CommandLine.Option(names = {"-f", "--format=format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
+
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
