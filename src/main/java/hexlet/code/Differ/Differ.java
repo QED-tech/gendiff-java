02a46717e1f42a20aea51a8d5447cf4d2ac678e4
qed <qedirast@gmail.com>
2022-01-17 22:51:11 +0700

    implemented parse json

--- src/main/java/hexlet/code/Differ/Differ.java
+++ src/main/java/hexlet/code/Differ/Differ.java
@@ -1,8 +1,30 @@
 package hexlet.code.Differ;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Differ {
 
-    public static String generate(String firstFile, String secondFile) {
+    public static String generate(String firstFilePath, String secondFilePath) throws Exception {
+        String contentFirstFile = Files.readString(new File(firstFilePath).toPath());
+        String contentSecondFile = Files.readString(new File(secondFilePath).toPath());
+
+        return Differ.getDiff(
+                Differ.getData(contentFirstFile),
+                Differ.getData(contentSecondFile)
+        );
+    }
+
+    private static Map getData(String content) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(content, TreeMap.class);
+    }
+
+    private static String getDiff(Map firstMap, Map secondMap) {
         return "result";
     }
 
